4e66b848b836a49b492801fcced2c57a76bb5d13
Gabriel Vasconcelos Auzier <gabriel@loldesign.com.br>
2023-09-25 18:38:58 -0400

    feat: add sidebar layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="align-center text-2 items-center justify-center font-bold">
-        Hello Next, with Tailwind!
-      </h1>
+      <h1>Hello Next, with Tailwind!</h1>
     </div>
   )
 }
